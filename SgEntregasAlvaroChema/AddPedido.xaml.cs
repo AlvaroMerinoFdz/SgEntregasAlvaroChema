99dba969400361f13cd35066c2b72a0f03d8077d
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2022-02-09 17:03:13 +0100

    Refactorizacion de errores

---
+++ SgEntregasAlvaroChema/AddPedido.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SgEntregasAlvaroChema
+{
+    /// <summary>
+    /// Lógica de interacción para AddPedido.xaml
+    /// </summary>
+    public partial class AddPedido : Window
+    {
+        public AddPedido()
+        {
+            InitializeComponent();
+        }
+    }
+}
